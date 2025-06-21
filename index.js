5c2dbb2021af21da9a722e20e09a1e636c6d3e14
lolodev <horaciodev1706@gmail.com>
2025-06-20 22:57:47 -0300

    Delete all data using remove

--- index.js
+++ index.js
@@ -1,40 +1,103 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+// import {
+//     getDatabase,
+//     ref,
+//     push,
+//     onValue
+// } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js";
+
+// const firebaseConfig = {
+//     databaseURL: "https://leads-tracker-app-18b45-default-rtdb.firebaseio.com/",
+// };
+
+// const app = initializeApp(firebaseConfig);
+// const database = getDatabase(app);
+// const referenceInDB = ref(database, "leads");
+
+// const inputEl = document.getElementById("input-el");
+// const inputBtn = document.getElementById("input-btn");
+// const ulEl = document.getElementById("ul-el");
+// const deleteBtn = document.getElementById("delete-btn");
+
+// function render(leads) {
+//     let listItems = "";
+//     for (let i = 0; i < leads.length; i++) {
+//         listItems += `
+//             <li>
+//                 <a target='_blank' href='${leads[i]}'>
+//                     ${leads[i]}
+//                 </a>
+//             </li>
+//         `;
+//     }
+//     ulEl.innerHTML = listItems;
+// }
+
+// // Challenge: Log out a snapshot of your database when a new value is added to it
+// onValue(referenceInDB, function (snapshot) {
+//     console.log(snapshot);
+// });
+
+// deleteBtn.addEventListener("dblclick", function () {
+
+// });
+
+// inputBtn.addEventListener("click", function () {
+//     push(referenceInDB, inputEl.value);
+//     inputEl.value = "";
+// });
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import {
     getDatabase,
     ref,
-    push
-} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js";
+    push,
+    onValue,
+    remove
+} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
 
 const firebaseConfig = {
-    databaseURL: "https://leads-tracker-app-18b45-default-rtdb.firebaseio.com/",
+    databaseURL: "https://leads-tracker-app-18b45-default-rtdb.firebaseio.com/"
 };
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const referenceInDB = ref(database, "leads");
 
-const inputEl = document.querySelector("#input-el");
-const inputBtn = document.querySelector("#input-btn");
-const ulEl = document.querySelector("#ul-el");
-const deleteBtn = document.querySelector("#delete-btn");
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
 
 function render(leads) {
     let listItems = "";
     for (let i = 0; i < leads.length; i++) {
-        listItems += `<li>
-                          <a target='_blank' href='${leads[i]}'>
-                             ${leads[i]}
-                          </a>
-                      </li>`;
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `;
     }
     ulEl.innerHTML = listItems;
 }
 
+// Challenge: Log out a snapshot of your database when a new value is added to it
+onValue(referenceInDB, function (snapshot) {
+    if (snapshot.exists()) {
+        const snapshotValues = snapshot.val();
+        const leads = Object.values(snapshotValues);
+        render(leads);
+    }
+});
+
 deleteBtn.addEventListener("dblclick", function () {
+    remove(referenceInDB);
+    ulEl.innerHTML = "";
 });
 
 inputBtn.addEventListener("click", function () {
     push(referenceInDB, inputEl.value);
     inputEl.value = "";
 });
-
